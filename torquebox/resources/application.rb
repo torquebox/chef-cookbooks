37c97e34dd29e5564236e37b3702804c74e8523e
Tom Brunoli <tominated@gmail.com>
2014-03-26 10:21:22 +1000

    Fixed env option

--- torquebox/resources/application.rb
+++ torquebox/resources/application.rb
@@ -1,4 +1,4 @@
 actions :deploy, :undeploy
 
 attribute :path, :kind_of => String, :required => true
-attribute :environment, :kind_of => String, :default => 'development'
+attribute :env, :kind_of => String, :default => 'development'
